2ed2f16e41945f764a6726ee593cc26b60a289b9
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-06-17 00:18:11 +0100

    update

--- app/lib/powerhouse.js
+++ app/lib/powerhouse.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllUsers = async () => {
   try {
-    const res = await axios.get("http://localhost:3000/api/users");
+    const res = await axios.get("https://dinereview.vercel.app/api/users");
     return res.data;
   } catch (error) {
     console.log(error);
@@ -11,7 +11,7 @@ export const getAllUsers = async () => {
 
 export const getAllRestaurants = async () => {
   try {
-    const res = await axios.get("http://localhost:3000/api/restaurants");
+    const res = await axios.get("https://dinereview.vercel.app/api/restaurants");
     return res.data;
   } catch (error) {
     console.log(error);
@@ -20,7 +20,7 @@ export const getAllRestaurants = async () => {
 
 export const registerNewUser = async (name, email, password) => {
   try {
-    const res = await axios.post("http://localhost:3000/api/users", {
+    const res = await axios.post("https://dinereview.vercel.app/api/users", {
       name,
       email,
       password,
@@ -33,7 +33,7 @@ export const registerNewUser = async (name, email, password) => {
 
 export const registerNewRestaurant = async (name, email, password, address, phone) => {
   try {
-    const res = await axios.post("http://localhost:3000/api/restaurants", {
+    const res = await axios.post("https://dinereview.vercel.app/api/restaurants", {
       name,
       email,
       password,
