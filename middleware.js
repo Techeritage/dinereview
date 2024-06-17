f6f945d8b80d101241bb8f5e597d394438eb2a9f
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-06-17 15:57:19 +0100

    updated middleware

--- middleware.js
+++ middleware.js
@@ -1,28 +1,38 @@
-// middleware.js
 import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req) {
-  const token = await getToken({ req });
+  try {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/", req.url));
+    if (!token) {
+      console.log('No token found, redirecting to home page.');
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    const { pathname } = req.nextUrl;
+
+    console.log(`Pathname: ${pathname}, Token Role: ${token.role}`);
+
+    if (pathname.startsWith("/admin") && token.role !== "admin") {
+      console.log('Unauthorized access to admin route, redirecting.');
+      return NextResponse.redirect(new URL("/auth/unauthorized", req.url));
+    } else if (
+      pathname.startsWith("/restaurant") &&
+      token.role !== "restaurant"
+    ) {
+      console.log('Unauthorized access to restaurant route, redirecting.');
+      return NextResponse.redirect(new URL("/auth/unauthorized", req.url));
+    } else if (pathname.startsWith("/user") && token.role !== "user") {
+      console.log('Unauthorized access to user route, redirecting.');
+      return NextResponse.redirect(new URL("/auth/unauthorized", req.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error('Error in middleware:', error);
+    return NextResponse.redirect(new URL("/auth/unauthorized", req.url));
   }
-
-  const { pathname } = req.nextUrl;
-
-  if (pathname.startsWith("/admin") && token.role !== "admin") {
-    return NextResponse.redirect(new URL("/auth/unauthorized", req.url));
-  } else if (
-    pathname.startsWith("/restaurant") &&
-    token.role !== "restaurant"
-  ) {
-    return NextResponse.redirect(new URL("/auth/unauthorized", req.url));
-  } else if (pathname.startsWith("/user") && token.role !== "user") {
-    return NextResponse.redirect(new URL("/auth/unauthorized", req.url));
-  }
-
-  return NextResponse.next();
 }
 
 export const config = {
