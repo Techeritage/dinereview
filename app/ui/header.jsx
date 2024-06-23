d16889b4cdc1332d3bc0ce52b38482024c708a2e
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-06-23 22:19:43 +0100

    update

--- app/ui/header.jsx
+++ app/ui/header.jsx
@@ -1,12 +1,13 @@
 "use client";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   return (
-    <nav className="px-[5%] lg:px-[10%] bk-image-content py-3 flex items-center justify-between">
+    <nav className="px-[5%] lg:px-[10%] bk-image-content py-5 flex items-center justify-between">
       {/*<h1 className={`${risque.className} text-xl tracking-wider`}>DineReview</h1>*/}
 
       <Image
@@ -16,7 +17,7 @@ export default function Header() {
         alt="logo"
         className="w-[150px] md:w-[180px]"
       />
-      {session && (
+      {status === "authenticated" ? (
         <Image
           src={session?.user?.profilePicture}
           width={50}
@@ -24,6 +25,10 @@ export default function Header() {
           alt="profile pics"
           className="rounded-full border-white border-4 h-[50px] object-cover"
         />
+      ) : (
+        <button className="whitespace-nowrap px-7 md:px-10 py-3 rounded-xl border-2 border-white text-white text-sm lg:text-base text-center font-semibold">
+          <Link href="/">Login</Link>
+        </button>
       )}
     </nav>
   );
