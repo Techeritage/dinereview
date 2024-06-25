e7d6b474adca7fd0b113b937786ef4dae042e731
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-06-25 15:08:25 +0100

    update

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -2,11 +2,19 @@ import Hero from "@/app/ui/hero";
 import FeaturedRestaurants from "@/app/ui/featuredRestaurants";
 import ReviewForm from "@/app/ui/reviewForm";
 import RecentReviews from "@/app/ui/recentReviews";
-import Image from "next/image";
+import { getAllRestaurants } from "../lib/powerhouse";
 
 export default async function UserPage() {
+  const data = await getAllRestaurants();
+
+  const res = data.data;
   return (
     <main className="relative">
+      <div>
+        {res.map((dat) => (
+          <p>{dat.name}</p>
+        ))}
+      </div>
       <section>
         <Hero />
       </section>
