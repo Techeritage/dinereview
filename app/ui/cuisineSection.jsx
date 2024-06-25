cb6edd1bc444bd4de3c76d0b5523dd3ff3982e8b
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-06-25 22:42:30 +0100

    update

--- app/ui/cuisineSection.jsx
+++ app/ui/cuisineSection.jsx
@@ -35,19 +35,11 @@ export default function CuisineSection() {
         <h2 className={`${risque.className} tracking-wide text-xl capitalize`}>
           Explore by Cuisine
         </h2>
-        <Link
-          href="/user/restaurant"
-          className="capitalize text-sm flex items-center gap-1"
-        >
-          see all{" "}
-          <span>
-            <ChevronRightIcon width={20} />
-          </span>
-        </Link>
       </div>
       <div className="px-[5%] mt-2 grid gap-5 grid-cols-2 md:grid-cols-4">
         {cuisines?.map((rest) => (
           <div key={rest.name} className="relative overflow-hidden h-[100px] rounded-lg w-full">
+            <h2 className="text-base font-semibold z-10 text-white absolute top-2 left-2">{rest.name}</h2>
             <Image
               src={rest.image}
               width={200}
@@ -55,7 +47,8 @@ export default function CuisineSection() {
               alt={`${rest.name} image`}
               className="h-full w-full object-cover"
             />
-            <div className="absolute top-0 bottom-0 right-0 left-0 bg-black/30"></div>
+            
+            <div className="absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-br from-black/80 to-black/30"></div>
           </div>
         ))}
       </div>
