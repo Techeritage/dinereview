be4803acbacdae4a0ac4bb52157fe869cb64623e
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-06-27 13:50:29 +0100

    update

--- app/ui/recentReviews.jsx
+++ app/ui/recentReviews.jsx
@@ -32,9 +32,9 @@ export default function RecentReviews({ reviews }) {
         {reviews?.map((review) => (
           <div
             key={review._id}
-            className="mt-2 shadow-sm py-3 px-1 bg-white rounded-xl"
+            className="mt-2 shadow-sm min-h-[200px] p-3 bg-white rounded-xl"
           >
-            <div className="flex items-center gap-2 mb-2 pl-2">
+            <div className="flex items-center gap-2 mb-4 pl-2">
               <Image
                 src={review?.restaurant?.profilePictureUrl}
                 width={20}
@@ -42,20 +42,20 @@ export default function RecentReviews({ reviews }) {
                 alt={`${review.restaurant?.name} logo`}
                 className="rounded-full"
               />
-              <p className="font-semibold text-[#333333] text-sm">
+              <p className="font-semibold text-[#333333]">
                 {review.restaurant?.name}
               </p>
             </div>
-            <div className="flex items-center gap-2 pl-2 mb-2">
+            <div className="flex items-center gap-2 pl-2 mb-4">
               <Image
                 src={review?.user?.profilePicture}
-                width={40}
-                height={40}
+                width={50}
+                height={50}
                 alt={`${review.restaurant?.name} logo`}
-                className="rounded-full h-[40px] object-cover"
+                className="rounded-full h-[50px] object-cover"
               />
-              <div>
-                <p className={`${risque.className} text-sm tracking-wide`}>
+              <div className="flex h-[45px] items-center flex-col justify-between">
+                <p className={`${risque.className} tracking-wide`}>
                   {review.user?.name}
                 </p>
                 <div className="flex items-center gap-3">
